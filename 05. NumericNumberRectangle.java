60129b6341661e95c89b86c1ad1584bb30e342c0
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-02 22:59:13 +0530

    Fix formatting issues in NumericNumberRectangle.java

--- 05. NumericNumberRectangle.java
+++ 05. NumericNumberRectangle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
         Scanner input = new Scanner(System.in);
         int r = input.nextInt();
         int c = input.nextInt();
-
+ 
         for(int i = 1; i <= r; i++){
             for(int j = 1; j <= c ; j++) {
                 if ((i + j) % 2 == 0) {
