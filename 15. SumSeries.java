5cdbdb2373305c87e7b187b2110effcf3720ccd4
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-06 08:40:50 +0530

    Update 15. SumSeries.java

--- 15. SumSeries.java
+++ 15. SumSeries.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number Till You Want Sum : ");
         int num = input.nextInt();
-
         int sum = 0;
         for( int i = 1 ; i <= num ; i++){
             if( i % 2 == 0){
