a3e71516548a8c39e73e0decd84d3b793b30ca2b
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-12 07:10:40 +0530

    Fix formatting issues in ReverseArray.java

--- 21.ReverseArray.java
+++ 21.ReverseArray.java
@@ -18,7 +18,6 @@ class ReverseArray {
             }
         return  ans;
     }
-
     public static void printarray(int[] arr){
         for(int i=0; i < arr.length; i++){
             System.out.print(arr[i]+" ");
