70bb2bd16918d7ff3a99868abc30bbe5cb690d9f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-12 07:10:27 +0530

    Update 21.ReverseArray.java

--- 21.ReverseArray.java
+++ 21.ReverseArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class ReverseArray {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,9};
         System.out.println("Original Array Is : ");
         printarray(arr);
