d8cdaf28fc9edb1af117d2a034b313b907f9df35
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-13 07:34:31 +0530

    Refactor reverse array implementation in place

--- 22.ReverseArrayInPlace.java
+++ 22.ReverseArrayInPlace.java
@@ -5,7 +5,6 @@ class ReverseArrayInPlace {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         reverse(arr);
         printarray(arr);
-
     }
     public static void printarray ( int[] arr){
         for (int i = 0; i < arr.length; i++) {
