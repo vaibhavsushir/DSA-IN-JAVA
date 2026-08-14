b5e1f6a2ebecef4ada192d7880f706d28dcfdfe0
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-14 07:34:23 +0530

    Refactor RotateArray for clarity and functionality

--- 23.RotateArray.java
+++ 23.RotateArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  class RotateArray {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int[] numarr = {1,2,3,4,5};
         System.out.print("Enter How Many Rotation You Perform: ");
