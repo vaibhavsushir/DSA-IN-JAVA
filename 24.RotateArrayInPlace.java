cc457558229bc5173dd5d974182b92b0f3c57f52
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-15 07:37:33 +0530

    Refactor RotateArrayInPlace class structure

--- 24.RotateArrayInPlace.java
+++ 24.RotateArrayInPlace.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- class RotateArrayInPlace {
+ class RotateArrayInPlace{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] arr = {1, 2, 3, 4, 5, 6};
