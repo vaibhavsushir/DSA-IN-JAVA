9e591be81ef7959f762159486562aa906c0bee58
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-16 09:27:30 +0530

    Add files via upload

---
+++ 25. SortZeroArray.java
@@ -0,0 +1,28 @@
+class SortZeroArray {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 1, 0, 1, 0, 0, 0, 1,0,0,1};
+        sort(arr);
+        ArrayUtility.printarray(arr);
+
+    }
+
+    static void sort(int[] arr) {
+        int zeroes = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                zeroes++;
+            }
+        }
+            for (int i = 0; i < n; i++) {
+                if (i < zeroes) {
+                    arr[i] = 0;
+                } else {
+                    arr[i] = 1;
+                }
+
+            }
+        }
+    }
+
+
