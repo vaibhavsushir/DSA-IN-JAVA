95d7e35118b861c8d27941260cce7f82768651e4
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-24 23:41:28 +0530

    Update 33.AdditionMatrices.java

--- 33.AdditionMatrices.java
+++ 33.AdditionMatrices.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- class AdditionMatrices{
+ class AdditionMatrices{ 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter First Matrix");
