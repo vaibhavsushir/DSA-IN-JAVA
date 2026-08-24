d37b134c1fa0127205645d32aa8d33f727ae1ea9
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-24 23:36:08 +0530

    Refactor AdditionMatrices for clarity and structure

--- 33.AdditionMatrices.java
+++ 33.AdditionMatrices.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- class AdditionMatrices {
+ class AdditionMatrices{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter First Matrix");
@@ -36,7 +36,6 @@ import java.util.Scanner;
         int[][] add = add(arr, r1, c1, arr2, r2, c2);
         printarr(add);
     }
-
             static int[][] add( int[][] a, int r1, int c1, int[][] b, int r2, int c2){
                 if (r1 != r2 || c1 != c2) {
                     System.out.println("Wrong Input Addition Is Not Possible");
