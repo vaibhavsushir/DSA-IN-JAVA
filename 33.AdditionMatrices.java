4a59500db724719e9fa93fb4fdad53ecbd84ecfd
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-24 23:35:51 +0530

    Update 33.AdditionMatrices.java

--- 33.AdditionMatrices.java
+++ 33.AdditionMatrices.java
@@ -52,8 +52,8 @@ import java.util.Scanner;
             }
 
             static void printarr ( int[][] array){
-                for (int i = 0; i < array.length; i++) {
-                    for (int j = 0; j < array[i].length; j++) {
+                for (int i = 0; i < array.length; i++){
+                    for (int j = 0; j < array[i].length; j++){
                         System.out.print(array[i][j] + " ");
                     }
                     System.out.println();
