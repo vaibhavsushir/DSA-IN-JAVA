5ca43b7006da82e492b0972acae9cb5f48233da3
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-27 23:12:19 +0530

    Update 36.Rotation_90.java

--- 36.Rotation_90.java
+++ 36.Rotation_90.java
@@ -18,13 +18,3 @@ class Rotation_90 {
             }
         }
     }
-
-    static void reversearray(int[] arr){
-        int i=0; int j= arr.length-1;
-
-        while(i<j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-     
