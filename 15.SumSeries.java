bb134bea9ade788db4266c49041819efa3c91aed
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-29 22:50:45 +0530

    Fix formatting issues in SumSeries.java

--- 15.SumSeries.java
+++ 15.SumSeries.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
         int sum = 0;
         for( int i = 1 ; i <= num ; i++){
             if( i % 2 == 0){
-                sum = sum - i ;
+                sum = sum - i;
             }else{
                 sum = sum + i;
             }
