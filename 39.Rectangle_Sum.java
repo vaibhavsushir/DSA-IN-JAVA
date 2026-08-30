35dd1d4a51d4bb9dd227c3de7d94945ff9dd3291
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-30 23:31:34 +0530

    Update 39.Rectangle_Sum.java

--- 39.Rectangle_Sum.java
+++ 39.Rectangle_Sum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
- class Rectangle_Sum {
+ class Rectangle_Sum{
      static void printarr(int[][] arr){
          for(int i=0; i< arr.length; i++){
              for(int j=0; j<arr[i].length; j++){
