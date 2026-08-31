d49e382733d604e7322329df08d24fb7ed949827
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-31 23:20:15 +0530

    Update 40. Reverse_List.java

--- 40. Reverse_List.java
+++ 40. Reverse_List.java
@@ -5,13 +5,7 @@ class Reverse_List {
 
     static void reverselist(ArrayList<Integer> list ) {
         int i = 0; int j =list.size()-1;
-        while(i < j){
-            Integer temp  = Integer.valueOf(list.get(i));
-            list.set(i, list.get(j));
-            list.set(j,temp);    public static void main(String[] args) {
-        list.add(0);
-        list.add(10);
-        list.add(3);
+        while(i < j)
         list.add(5);
         list.add(22);
         list.add(10);
