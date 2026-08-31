fd2d2ea64b59b351ddb18605752bf5b91259428f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-31 23:19:48 +0530

    Update 40. Reverse_List.java

--- 40. Reverse_List.java
+++ 40. Reverse_List.java
@@ -12,8 +12,6 @@ class Reverse_List {
             i++;
             j--;
         }
-
-    }
     public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<>();
         list.add(0);
