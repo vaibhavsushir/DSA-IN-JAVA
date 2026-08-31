3b92c4ff3116adbabef722203eefaaff2a1afe1e
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-31 23:22:27 +0530

    Update 01. PrintPattern.java

--- 01. PrintPattern.java
+++ 01. PrintPattern.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
-class PrintPattern {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int r = input.nextInt
-            for( int j =1; j <= c;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+class PrintPattern 
         }
         System.out.println("For Boarder");
         for (int i = 1; i <= r; i++) {
