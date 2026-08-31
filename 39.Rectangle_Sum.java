aedb4ccdb9dabc9cd33f7a5146444efe9b69bb9f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-31 23:15:47 +0530

    Update 39.Rectangle_Sum.java

--- 39.Rectangle_Sum.java
+++ 39.Rectangle_Sum.java
@@ -11,11 +11,6 @@ import java.util.Scanner;
      static int findsum(int[][] matrix, int l1, int r1, int l2, int r2){
      int sum = 0;
      for(int i=l1; i<=l2; i++){
-         for(int j=r1; j<=r2; j++){ 
-             sum += matrix[i][j];
-         }
-    return sum;
-     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Row: ");
