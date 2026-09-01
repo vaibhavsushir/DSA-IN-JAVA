4d4ffe948d18a060e1428c0b473ec8c66c0ba447
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-01 23:39:34 +0530

    Update 01.PrintPattern.java

--- 01.PrintPattern.java
+++ 01.PrintPattern.java
@@ -9,9 +9,7 @@ class PrintPattern {
         for(int i = 1; i <= r; i++ ){
             for( int j =1; j <= c;j++){
                 System.out.print("*");
-            }
-            System.out.println();
-        }
+            }        }
                 for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= c; j++) {
                 if (i == 1 || i == r || j == 1 || j == c){
