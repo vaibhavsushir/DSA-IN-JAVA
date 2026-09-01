66517eedd73605c497f68614c000783068c79568
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-01 23:53:00 +0530

    Update 53. Array_Recursion.java

--- 53. Array_Recursion.java
+++ 53. Array_Recursion.java
@@ -16,14 +16,6 @@ class Array_Recursion {
         int smallans = max(arr,idx+1);
 
         return Math.max(arr[idx],smallans);
-    }
-    public static void main(String[] args) {
-        int[] arr = {5, 6, 7, 18, 9,4,3,2};
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter idx Number: ");
-        int idx = input.nextInt();
-        System.out.println("Array Is ");
-        arrprint(arr, idx);
         int max = max(arr,idx);
         System.out.println("Maximum Is "+max);
     }
