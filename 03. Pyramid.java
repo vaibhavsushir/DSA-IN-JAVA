0d1ccdc684f724c4a0f1bb04aaec5367590bd420
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-01 23:47:02 +0530

    Update 03. Pyramid.java

--- 03. Pyramid.java
+++ 03. Pyramid.java
@@ -10,9 +10,7 @@ class Pyramid {
             }
             for(int k = 1; k <= (2*i)-1 ; k++){
                 System.out.print("*");
-            }
-            System.out.println();
-        }
+            }        }
 
     }
 }
