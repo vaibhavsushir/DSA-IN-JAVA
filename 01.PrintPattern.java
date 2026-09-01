c38b2ffea63749e04433b1b5e6a59e369d1bbbea
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-01 23:40:09 +0530

    Update 01.PrintPattern.java

--- 01.PrintPattern.java
+++ 01.PrintPattern.java
@@ -5,9 +5,7 @@ class PrintPattern {
         Scanner input = new Scanner(System.in);
         int r = input.nextInt();
         int c = input.nextInt();
-        System.out.println("For Rectagular\n");
-        for(int i = 1; i <= r; i++ ){
-            for( int j =1; j <= c;j++){
+        System.out.println("For Rectagular\n");            for( int j =1; j <= c;j++){
                 System.out.                for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= c; j++) {
                 if (i == 1 || i == r || j == 1 || j == c){
