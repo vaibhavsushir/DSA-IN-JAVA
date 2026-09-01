157974e53eb904280310e13be26b384c2921034f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-01 23:28:14 +0530

    Add files via upload

---
+++ 45. Fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Fibonacci {
+    static int fib(int num){
+        //Base Case
+        if(num == 0 || num ==1){
+            return num;
+        }
+        //Subproblem
+        int prev = fib(num-1);
+        int prevprev = fib(num-2);
+        //Selfwork
+        return prev + prevprev;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = input.nextInt();
+        for(int i=0 ;i<=num; i++) {
+            System.out.println(fib(i));
+        }
+        }
+}
