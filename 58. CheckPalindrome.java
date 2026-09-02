503099e17daf4fbddb8ddaba05acd1585454ceef
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-02 22:27:58 +0530

    Update 58. CheckPalindrome.java

--- 58. CheckPalindrome.java
+++ 58. CheckPalindrome.java
@@ -3,8 +3,6 @@ class CheckPalindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter String: ");
-        String str = input.next();
-        String rev = reverse(str,0);
         if(rev.equals(str)){
             System.out.printf("%s Is Palindrome ",str);
         }else{
