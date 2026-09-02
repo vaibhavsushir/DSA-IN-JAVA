50d60f11af913e1ff425c3e708eda205d3cd1d08
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-02 22:28:30 +0530

    Update 58. CheckPalindrome.java

--- 58. CheckPalindrome.java
+++ 58. CheckPalindrome.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 class CheckPalindrome {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         System.out.print("Enter String: ");
         if(rev.equals(str))ome ",str);
         }else{
