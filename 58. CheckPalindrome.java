67da36f21ea1961ee2afeddbb378cbaecb5d046c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-02 22:28:49 +0530

    Update 58. CheckPalindrome.java

--- 58. CheckPalindrome.java
+++ 58. CheckPalindrome.java
@@ -3,6 +3,5 @@ class CheckPalindrome {
         System.out.print("Enter String: ");
         if(rev.equals(str))ome ",str);
         }else{
-            System.out.printf("%s Is Not Palindrome ",str);
     }
 }
