a9c6af7eb7f1debebf9a1c1d702d976610154bd4
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-02 22:35:43 +0530

    Delete 60. ReurnSSQ.java

--- 60. ReurnSSQ.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-class ReurnSSQ {
-
-    static ArrayList<String> getSSQ(String s){ayList<>();
-        if(s.length
-            System.out.println(ss);
-    }
-}
