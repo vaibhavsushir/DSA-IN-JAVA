f047d0599fabdfa6aaa375aa5df512d2cc7000e7
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-02 22:35:16 +0530

    Update 60. ReurnSSQ.java

--- 60. ReurnSSQ.java
+++ 60. ReurnSSQ.java
@@ -4,18 +4,6 @@ class ReurnSSQ {
 
     static ArrayList<String> getSSQ(String s){ayList<>();
         if(s.length
-        char curr = s.charAt(0);
-        ArrayList<String> smallans = getSSQ(s.substring(1));
-        for(String ss : smallans){
-            ans.add(ss);
-            ans.add(curr+ss);
-        }
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        ArrayList<String> list = getSSQ("abc");
-        for(String ss: list){
             System.out.println(ss);
         }
     }
