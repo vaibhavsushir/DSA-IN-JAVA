1f33ed0d13aaaae0decfa7275f98d501aaaa3e8f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-03 22:42:19 +0530

    Update 59. PalindromeTwoPointer.java

--- 59. PalindromeTwoPointer.java
+++ 59. PalindromeTwoPointer.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 class PalindromeTwoPointer {
 
     static boolean ispalindrome(String str, int l, int r) {
-        if (l >= r) {
-            return true;
-        }
         if (str.charAt(l) == str.charAt(r) && pal) {
             return true;
         }
