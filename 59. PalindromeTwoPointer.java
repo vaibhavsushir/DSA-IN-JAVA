48b0ccdd618da67bbeb977baf373a9d17efa6f72
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-03 22:44:54 +0530

    Update 59. PalindromeTwoPointer.java

--- 59. PalindromeTwoPointer.java
+++ 59. PalindromeTwoPointer.java
@@ -4,7 +4,6 @@ class PalindromeTwoPointer {
     static boolean ispalindrome(String str, int l, int r) {
         if (str.charAt(l) == str.charAt(r) && pal) {
         
-    }
 
         String str = input.nex
         int n = str.
