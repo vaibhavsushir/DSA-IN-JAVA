a0386b1fc6908f6961d4cab3551d4b723e2c0360
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-03 22:50:36 +0530

    Update 58. CheckPalindrome.java

--- 58. CheckPalindrome.java
+++ 58. CheckPalindrome.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 class CheckPalindrome {
 
     static String reverse(String str,int idx){
-        if(idx == str.length()) return "";
-
-        String smal
+        if(idx == str.length()) retu
 
     public static void main(String[] args) {
         System.out.print("Enter String: ");
