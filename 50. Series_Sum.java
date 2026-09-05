57bcccc9d4a2ac596a386dff461c4dbc8afb0a2b
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-05 22:50:54 +0530

    Add files via upload

---
+++ 50. Series_Sum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Series_Sum {
+
+    static int seriessum1(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num % 2 == 0) {
+            return seriessum1(num - 1) - num;
+        } else {
+            return seriessum1(num - 1) + num;
+        }
+    }
+//static int seriessum(int num){
+//    if(num==0){
+//        return 0;
+//    }
+//    return seriessum(num-1)+num;
+//}
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num =input.nextInt();
+        int sum = seriessum1(num);
+        System.out.println("Series Sum Is "+sum);
+    }
+}
