133c6c5a5aaf793a7f66684f54d41f0e14c51d58
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-06 20:26:47 +0530

    Refactor StoreIdxArrayList.java code structure

--- 55. StoreIdxArrayList.java
+++ 55. StoreIdxArrayList.java
@@ -13,17 +13,3 @@ import java.util.ArrayList;
         ArrayList<Integer> smallans = AllIndice(arr, target, idx + 1);
         ans.addAll(smallans);
         return ans;
-    }
-
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[] arr = {1, 2, 3,1, 4, 5,1,4};
-        System.out.print("Enter Target: ");
-        int target = input.nextInt();
-        System.out.print("Enter idx: ");
-        int idx = input.nextInt();
-        ArrayList<Integer> AI=AllIndice(arr, target, idx);
-        printarr(AI);
-    }
-}
