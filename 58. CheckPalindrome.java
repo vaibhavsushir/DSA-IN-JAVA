1a9081b6ecda81da6e63649f520d73e5dcaec8ae
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-06 20:27:17 +0530

    Update 58. CheckPalindrome.java

--- 58. CheckPalindrome.java
+++ 58. CheckPalindrome.java
@@ -7,18 +7,6 @@ class CheckPalindrome {
         String smallans = reverse(str,idx+1);
         char currchar = str.charAt(idx);
         return smallans+currchar;
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter String: ");
-        String str = input.next();
-        String rev = reverse(str,0);
-        if(rev.equals(str)){
-            System.out.printf("%s Is Palindrome ",str);
-        }else{
-            System.out.printf("%s Is Not Palindrome ",str);
-        }
-
+    
     }
 }
