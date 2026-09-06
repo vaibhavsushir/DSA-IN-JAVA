f9a8a6de5ac91cfca3bca8cfd42b618296e32781
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-06 20:26:04 +0530

    Refactor Recursion_Basic.java for clarity

--- 43.Recursion_Basic.java
+++ 43.Recursion_Basic.java
@@ -11,23 +11,6 @@ class Recursion_Basic {
             fib(n);
         }
 
-//    static void PrintDecreasing(int num){
-//        if(num == 1){
-//            System.out.println("1");
-//            return;
-//        }
-//        System.out.println(num);
-//        PrintDecreasing(num-1);
-//    }
-//    static void PrintIncreasing(int num){
-//        if(num == 1){
-//            System.out.println("1");
-//            return;
-//        }
-//        PrintIncreasing(num-1);
-//        System.out.println(num);
-//    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number: ");
