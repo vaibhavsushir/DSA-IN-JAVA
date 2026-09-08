b469502bc44cd150a37e3225c24edb7ab137d6e5
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-08 23:13:51 +0530

    Update 51.GCD_Basic.java

--- 51.GCD_Basic.java
+++ 51.GCD_Basic.java
@@ -13,12 +13,4 @@ class GCD_Basic {
 }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter 1st Number: ");
-        int num1 =input.nextInt();
-        System.out.print("Enter 2nd Number: ");
-        int num2 =input.nextInt();
-        int gcd = gcd(num1,num2);
-        System.out.println("GCD Is "+ gcd);
-
-    }
-}
+        System.out.p
