f601bc6f3f31a9b5bbe8ee2d360bf40212d16588
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-08 23:10:07 +0530

    Update 45.Fibonacci.java

--- 45.Fibonacci.java
+++ 45.Fibonacci.java
@@ -11,7 +11,6 @@ class Fibonacci {
         //Selfwork
         return prev + prevprev;
     }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number: ");
