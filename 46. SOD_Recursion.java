4ce328d7488ddddc9a4c903eafa80921d6dc92c8
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-10 23:14:17 +0530

    Add files via upload

---
+++ 46. SOD_Recursion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SOD_Recursion {
+
+    static int sod(int num){
+        //Base Case
+        if(num >= 0 && num <= 9){
+            return num;
+        }
+        //Recursion Work
+        int smallans = sod(num / 10);
+
+        //Self Work
+        int ans = smallans + num % 10;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = input.nextInt();
+    }
+}
