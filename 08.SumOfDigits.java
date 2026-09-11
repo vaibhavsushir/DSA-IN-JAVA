b446ea6e7db433c0aaff2f2b219d50103aaa5975
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-11 23:16:03 +0530

    Update 08.SumOfDigits.java

--- 08.SumOfDigits.java
+++ 08.SumOfDigits.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
- class SumOfDigits {
+ class SumOfDigits{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Your Number: ");
