ce88c9095d575810c21ea5085f50d8e4444c39c6
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-14 23:17:58 +0530

    Format code and fix newline at end of file

--- 50.Series_Sum.java
+++ 50.Series_Sum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Series_Sum{
 
-    static int seriessum1(int num) {
+    static int seriessum1(int num){
         if (num == 0) {
             return 0;
         }
@@ -11,7 +11,7 @@ class Series_Sum{
             return seriessum1(num - 1) + num;
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Number: ");
         int num =input.nextInt();
