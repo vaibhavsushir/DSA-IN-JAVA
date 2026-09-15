c9645791b46ad01620a8474f65be6ffeccf98e1c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-15 23:22:09 +0530

    Update 23.RotateArray.java

--- 23.RotateArray.java
+++ 23.RotateArray.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
  class RotateArray {
     public static void main(String[] args){
+     System.out.print("Rotate Array /n");
         Scanner input = new Scanner(System.in);
         int[] numarr = {1,2,3,4,5};
         System.out.print("Enter How Many Rotation You Perform: ");
