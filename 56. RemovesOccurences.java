4fd8655803d4743dd430736ee4b410a61635796d
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-20 11:39:09 +0530

    Add files via upload

---
+++ 56. RemovesOccurences.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+class RemovesOccurences {
+
+    static String remove(String s,int idx){
+        int n = s.length();
+        String ans;
+        if(idx == n){
+            return " ";
+        }
+        String smallans = remove(s,idx+1);
+        char currchar = s.charAt(idx);
+        if(currchar != 'a'){
+            return currchar+smallans;
+        }else{
+            return smallans;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String s = input.nextLine();
+        System.out.println(remove(s,0));
+    }
+}
