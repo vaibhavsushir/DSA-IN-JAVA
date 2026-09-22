86fd4d8be6105aca8e5a54c97f5172a943b078e8
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-22 17:03:59 +0530

    Add files via upload

---
+++ 58. CheckPalindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class CheckPalindrome {
+
+    static String reverse(String str,int idx){
+        if(idx == str.length()) return "";
+
+        String smallans = reverse(str,idx+1);
+        char currchar = str.charAt(idx);
+        return smallans+currchar;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = input.next();
+        String rev = reverse(str,0);
+        if(rev.equals(str)){
+            System.out.printf("%s Is Palindrome ",str);
+        }else{
+            System.out.printf("%s Is Not Palindrome ",str);
+        }
+
+    }
+}
