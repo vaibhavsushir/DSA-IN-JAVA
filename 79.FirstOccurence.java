44d06f91deea3ad7a4ac5604fd68abea3970fe21
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-23 21:45:15 +0530

    Update 79.FirstOccurence.java

--- 79.FirstOccurence.java
+++ 79.FirstOccurence.java
@@ -1,8 +1,6 @@
 class FirstOccurence {
 
         public static void main(String[] args) {
-            int[] arr = {1,2,3,4,5,7,8};
-           int fo = search(arr,8);
-            System.out.println(fo);
+
     }
 }
