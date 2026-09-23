136efdb6d9a396a33be7119065dfbc98612a3856
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-23 21:26:53 +0530

    Add files via upload

---
+++ 60. ReurnSSQ.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class ReurnSSQ {
+
+    static ArrayList<String> getSSQ(String s){
+        ArrayList<String> ans = new ArrayList<>();
+        if(s.length() == 0){
+            ans.add(" ");
+            return ans;
+        }
+        char curr = s.charAt(0);
+        ArrayList<String> smallans = getSSQ(s.substring(1));
+        for(String ss : smallans){
+            ans.add(ss);
+            ans.add(curr+ss);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> list = getSSQ("abc");
+        for(String ss: list){
+            System.out.println(ss);
+        }
+    }
+}
