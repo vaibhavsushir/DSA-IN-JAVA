ea78b4fa6ed1406a779c350bd0d6d07d35581915
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-23 21:26:32 +0530

    Add files via upload

---
+++ 57. ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ReverseString {
+    static String reverse(String str,int idx){
+        if(idx == str.length()) return " ";
+        String smallans = reverse(str,idx+1);
+        return smallans+str.charAt(idx);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = input.next();
+        String rev = reverse(str,0);
+        System.out.println("Reverse String Is "+rev);
+
+    }
+}
