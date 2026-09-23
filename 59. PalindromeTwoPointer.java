c33e09cdd2b49285f76c2cb32c1a81b001fa96cb
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-23 21:20:17 +0530

    Add files via upload

---
+++ 59. PalindromeTwoPointer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class PalindromeTwoPointer {
+
+    static boolean ispalindrome(String str, int l, int r) {
+        if (l >= r) {
+            return true;
+        }
+        boolean pal = ispalindrome(str, l + 1, r - 1);
+        if (str.charAt(l) == str.charAt(r) && pal) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = input.next();
+        int n = str.length();
+        boolean pal = ispalindrome(str, 0, n - 1);
+        if (pal) {
+            System.out.printf("%s Is Palindrome ", str);
+        } else {
+            System.out.printf("%s Is Not Palindrome ", str);
+        }
+    }
+}
