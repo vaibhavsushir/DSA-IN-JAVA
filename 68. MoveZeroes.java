2e837a627f84994bc357aa8ee0c7b7d73ced7aea
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-23 21:36:44 +0530

    Add files via upload

---
+++ 68. MoveZeroes.java
@@ -0,0 +1,21 @@
+ class MoveZeroes {
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] == 0 && arr[j + 1] != 0) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,10,5,0,4};
+        sort(arr);
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
