73b95420cea9328ac3f4bebdd0acecf8bdd9d8fc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-23 21:46:07 +0530

    Update 76. SortPosNegNum.java

--- 76. SortPosNegNum.java
+++ 76. SortPosNegNum.java
@@ -1,15 +1,4 @@
 class SortPosNegNum {
-
- 
-                arr[r] = temp;
-                l++;
-                r--;
-            }
-
-        }
-    }
-
-
     public static void main(String[] args) {
         int[] num ={-13,20,7,0,-4,-13,11,-5,-13};
         sort(num);
