57635d721c9b2a2c61f9d49096dd44c4a3fb695d
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 20:25:55 +0530

    Update 73. Radix_Sort.java

--- 73. Radix_Sort.java
+++ 73. Radix_Sort.java
@@ -1,11 +1,1 @@
 
-    public static void main(String[] args) {
-        int[] arr = {1, 4, 5, 2, 2, 5, 6};
-        System.out.println("Original Array:");
-        displayArr(arr);
-        radixsort(arr);
-        System.out.println();
-        System.out.println("After Count Sort:");
-        displayArr(arr);
-    }
-}
