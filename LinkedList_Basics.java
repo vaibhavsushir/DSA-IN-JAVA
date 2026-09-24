6c7dfb0e8a00f2432f81dc3f6e4514907915fb92
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 21:40:31 +0530

    Add files via upload

---
+++ LinkedList_Basics.java
@@ -0,0 +1,33 @@
+public class LinkedList_Basics {
+    public static void display(node head){
+        node temp = head.next;
+        while(temp != null){
+            System.out.println(temp.data+" ");
+            temp = temp.next;
+        }
+    }
+
+    public static class node{
+        int data;
+        node next;
+        node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        node a = new node(5);
+        System.out.println(a.next);
+        node b = new node(4);
+        node c = new node(7);
+        node d = new node(8);
+        node e = new node(2);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+//        System.out.println(a.next);
+        display(a);
+    }
+
+}
