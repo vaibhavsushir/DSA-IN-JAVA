4ca7a2241c13ecfc4f679514b7ee995053fa7682
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 20:25:42 +0530

    Refactor Radix_Sort class structure

--- 73. Radix_Sort.java
+++ 73. Radix_Sort.java
@@ -1,17 +1,4 @@
-class Radix_Sort {
-    static void displayarr(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-    }
-        // Build output (stable sorting, go from right to left)
-    
-static void radixsort(int[] arr) {
-    int max = max(arr);
-    for (int place = 1; max / place > 0; place *= 10) {
-        countSort(arr, place);
-    }
-}
+
     public static void main(String[] args) {
         int[] arr = {1, 4, 5, 2, 2, 5, 6};
         System.out.println("Original Array:");
