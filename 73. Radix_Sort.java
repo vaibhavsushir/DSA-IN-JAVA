672d9846372fdb66d44951aa0c70cea62e8c41cc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 20:25:26 +0530

    Update 73. Radix_Sort.java

--- 73. Radix_Sort.java
+++ 73. Radix_Sort.java
@@ -5,23 +5,7 @@ class Radix_Sort {
         }
     }
         // Build output (stable sorting, go from right to left)
-        for (int i = n - 1; i >= 0; i--) {
-            int digit = (arr[i] / place) % 10;
-            output[count[digit] - 1] = arr[i];
-            count[digit]--;
-        }
-
-        // Copy output back to arr
-        for (int i = 0; i < n; i++) {
-            arr[i] = output[i];
-        }
-    }
-    static void displayArr(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
+    
 static void radixsort(int[] arr) {
     int max = max(arr);
     for (int place = 1; max / place > 0; place *= 10) {
