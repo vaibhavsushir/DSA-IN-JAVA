22c7bcadb3168c3f0474950a7a997b9c5707ed0e
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 20:37:10 +0530

    Add files via upload

---
+++ 86. StringCompression.java
@@ -0,0 +1,24 @@
+ class StringCompression {
+
+
+    public static void main(String[] args) {
+        String str ="aaabbcddeee";
+        String ans =" "+ str.charAt(0);
+        int count = 1;
+        for(int i=1;i<str.length();i++) {
+            char curr = str.charAt(i);
+            char prev = str.charAt(i - 1);
+            if (curr == prev) {
+                count++;
+            } else {
+                if (count > 1) ans += count;
+                count = 1;
+                ans += curr;
+
+            }
+        }
+            if(count>1) ans+=count;
+            System.out.println(ans);
+        }
+    }
+
