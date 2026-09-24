32bf0240db2a4ac0db3335bc4f0bdc98b7cff1c5
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 22:18:19 +0530

    Update 07.UpperPyramid.java

--- 07.UpperPyramid.java
+++ 07.UpperPyramid.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= (r + 1 - i); j++) {
                     System.out.print("*");
-                }
-                    System.out.println();
-                }
+               
             }
     }
