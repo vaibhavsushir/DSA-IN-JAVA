8a0472bbe71802713c898ab73b6ed899909ad5cf
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 20:24:16 +0530

    Update 78(A). Binary_Search.java

--- 78(A). Binary_Search.java
+++ 78(A). Binary_Search.java
@@ -1,7 +1,1 @@
- g[] args) {
-         int[] arr ={1,3,6,7,8,9,12,25,37,64,82,91,94};
-         int target = 37;
-         boolean BinarySearch =BinarySearch(arr,target);
-         System.out.println("Target Is Ans "+BinarySearch);
-     }
-}
+ 
