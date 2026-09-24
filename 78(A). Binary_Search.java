df1c300539d4cfe97c8a67d694b34f61cd20ea66
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 20:24:04 +0530

    Update 78(A). Binary_Search.java

--- 78(A). Binary_Search.java
+++ 78(A). Binary_Search.java
@@ -1,22 +1,4 @@
- class Binary_Search {
-    static boolean BinarySearch(int[] arr,int target){
-        int n = arr.length;
-        int st=0,end=n-1;
-        while(st<=end){
-            int mid = st + (end-st)/2;
-            if(arr[mid] == target){
-                return true;
-            }else if(target < arr[mid]){
-                end = mid-1;
-            }else{
-                st=mid+1;
-            }
-        }
-
-        return false;
-    }
-
-     public static void main(String[] args) {
+ g[] args) {
          int[] arr ={1,3,6,7,8,9,12,25,37,64,82,91,94};
          int target = 37;
          boolean BinarySearch =BinarySearch(arr,target);
