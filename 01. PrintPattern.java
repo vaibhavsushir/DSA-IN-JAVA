604c4678e2e326c910ec3f0e120b9427bbbd2039
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 21:38:19 +0530

    Update 01. PrintPattern.java

--- 01. PrintPattern.java
+++ 01. PrintPattern.java
@@ -1,16 +1,6 @@
 import java.util.Scanner;
 class PrintPattern {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int r = input.nextInt();
-        int c = input.nextInt();
-        System.out.println("For Rectagular\n");
-        for(int i = 1; i <= r; i++ ){
-            for( int j =1; j <= c;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
         System.out.println("For Boarder");
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= c; j++) {
