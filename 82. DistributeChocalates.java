1bcf0d3f975c4bd9fbe45a85ea8358ed56ce85d5
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-25 23:04:04 +0530

    Refactor DistributeChocalates class structure
    Removed unnecessary code and fixed formatting issues.

--- 82. DistributeChocalates.java
+++ 82. DistributeChocalates.java
@@ -1,24 +1,6 @@
 class DistributeChocalates {
 
     static boolean isdivisionposible(int[] a,int m,int mxchocalates){
-   
-
-    static int distributechocalate(int[] arr,int m){
-    if(arr.length < m) return -1;
-    int ans=0,st=1,end=(int)1e9;
-
-    while(st <= end){
-        int mid = st + (end-st)/2;
-
-        if(isdivisionposible(arr,m,mid)){
-            ans = mid;
-            end = mid-1;
-        }else{
-            st=mid+1;
-        }
-    }
-    return ans;
-    }
 
     public static void main(String[] args) {
         int[] arr ={12,34,67,90};
