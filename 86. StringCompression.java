1cdbb3718ab1bc67b01a9f3af7c706038a1f9b17
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-25 23:03:08 +0530

    Update 86. StringCompression.java

--- 86. StringCompression.java
+++ 86. StringCompression.java
@@ -2,23 +2,5 @@
 
 
     public static void main(String[] args) {
-        String str ="aaabbcddeee";
-        String ans =" "+ str.charAt(0);
-        int count = 1;
-        for(int i=1;i<str.length();i++) {
-            char curr = str.charAt(i);
-            char prev = str.charAt(i - 1);
-            if (curr == prev) {
-                count++;
-            } else {
-                if (count > 1) ans += count;
-                count = 1;
-                ans += curr;
-
-            }
-        }
-            if(count>1) ans+=count;
-            System.out.println(ans);
-        }
-    }
+      
 
