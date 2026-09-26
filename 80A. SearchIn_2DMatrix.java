e5998d8a34334b025444c317f0c058f33ac1a538
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-26 18:49:11 +0530

    Update 80A. SearchIn_2DMatrix.java

--- 80A. SearchIn_2DMatrix.java
+++ 80A. SearchIn_2DMatrix.java
@@ -1,10 +1,4 @@
 class SearchIn_2DMatrix {
 
     static boolean search(int[][] arr,int target){
-
-    public static void main(String[] args) {
-        int[][] arr ={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        boolean isfound = search(arr,16);
-        System.out.println(isfound);
-    }
 }
