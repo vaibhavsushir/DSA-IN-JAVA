065c52830091cf4c27db46776678418e28c98268
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-26 18:51:04 +0530

    Update 76.SortPosNegNum.java

--- 76.SortPosNegNum.java
+++ 76.SortPosNegNum.java
@@ -1,23 +1,6 @@
 class SortPosNegNum {
 
     static void displayarr(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-    }
-    static void sort(int[] arr){
-        int l=0,r= arr.length-1;
-        while (l<r) {
-            while(arr[l] < 0) l++;
-            while (arr[r] >= 0) r--;
-            if(l<r){
-                int temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
-                l++;
-                r--;
-            }
-
         }
     }
 
